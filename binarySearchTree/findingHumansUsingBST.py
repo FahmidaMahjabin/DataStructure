1dfbbe90294e3bf00819cd96a0f5c2bfcd0e7a44
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2021-10-24 23:58:14 +0600

    assign human using BST

--- binarySearchTree/findingHumansUsingBST.py
+++ binarySearchTree/findingHumansUsingBST.py
@@ -1,17 +1,50 @@
-from BST import BinarySearchTree
+from BST import BinarySearchTree, Node
 import sys
-sys.path.insert(1, "D:/Coding/allGitProject/DataStructure/hashTable/classUsingHash")
-from humanClass import *
-# function = assign age wise human 
-# input = humans 
-# output = None 
-# step1:ekta huamn nibo, human er age k node key hishebe nibo 
-# jodi human age ta theke thake tahole node e append korbo 
-# na thakle new node create kore assign korbo
-class Node:
-    def __init__(self, key):
-        self.key = key 
-        self.value = []
-        self.leftChild = None 
-        self.rightChild = None
+sys.path.append("d:\\Coding\\allGitProject\\DataStructure")
+from DataStructure.hashTable.classUsingHash.humanClass import *
+# from DataStructure.hashTable.classUsingHash.humanClass import *
+print(sys.path)
+class HumanNode(Node):
+    def __init__(self, key): 
+        self.humans = []
+        super().__init__(key)
         
+        
+# human1 = Human("eva", 25, "engineer", 2000)
+# eva = HumanNode( human1.age)
+
+
+class HumanSearchTree(BinarySearchTree):
+    """function  = search human
+    input =key (it could be agevalue, job title )
+    outPut =return node  or False 
+    """
+    """function = assign human with age 
+    input = human age 
+    output = none
+    step1:human age k key hishebe pathabo and check korbo j oi human ache kina 
+    step2:jodi theke thake then oi human k node er humans e append korbo 
+    step3:jodi human na thake then age wise human assign korbo 
+    """
+    def assignHumanWithAge(self, human):
+        existingHuman = self.hasValue(human.age)
+        if existingHuman:
+            existingHuman.humans.append(human)
+            
+        else:
+            self.insertValue(human.age)
+            self.assignHumanWithAge(human)
+            
+
+
+class Rectangle:  
+    def __init__(self, side):
+        self.side = side      
+
+class Triangle(Rectangle):
+    def __init__(self,base, largeSide):
+        self.base = base
+        super().__init__(largeSide)
+triA = Triangle(3, 2)
+print(triA.side)
+
